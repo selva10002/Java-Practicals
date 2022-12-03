3c4bdb6b8c1608e042b50ff9c50894a2b4f21431
selva10002 <119669635+selva10002@users.noreply.github.com>
2022-12-02 23:19:30 -0800

    Add files via upload

---
+++ Data Types/Hello_World.java
@@ -0,0 +1,38 @@
+package basic;
+
+public class Hello_World {
+	public static void main(String[] args) {
+		//boolean example
+		boolean IoT =true;
+	System.out.println(IoT);
+	//byte example
+	byte range;
+	range = 127;
+	System.out.println(range);
+	//short example
+	short range1;
+	range1 = -127;
+	System.out.println(range1);
+	//int example
+	int range2 = 300000000;
+	System.out.println(range2);
+	//long example
+	long range3 = 30000000000L;
+	System.out.println(range3);
+	//double example
+	double number = 99.9;
+	System.out.println(number);
+    //float example
+	float number1 = 99.9f;
+	System.out.println(number1);
+	//char example
+	char letter = '\u0041';
+	System.out.println(letter);
+	char letter2 = 65;
+	System.out.println(letter2);
+	char letter3 = 'A';
+	System.out.println(letter3);
+	
+	
+	}
+}
