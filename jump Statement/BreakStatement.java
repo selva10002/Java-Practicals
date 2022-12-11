8ac6c7db454ac8ea05f7a555880bda0141134be5
selva10002 <119669635+selva10002@users.noreply.github.com>
2022-12-11 17:49:59 +0530

    Add files via upload

---
+++ jump Statement/BreakStatement.java
@@ -0,0 +1,22 @@
+package jumpstatement;
+
+public class BreakStatement {
+
+	public static void main(String[] args)
+	{
+	  int i=1;
+	  while(i<10)
+	  {
+		  System.out.println("the number is"+i);
+          i++;
+	  
+	  if(i==5)
+	  {
+		 break;
+	  }
+
+	}
+	}
+
+}
+
---
+++ jump Statement/ContinueStatement.java
@@ -0,0 +1,19 @@
+package jumpstatement;
+
+public class ContinueStatement {
+
+	public static void main(String[] args) 
+	{
+		int a=1;
+		for(a=1;a<10;a++)
+		{
+			
+		if(a%2==1)
+		{
+			continue;
+		}
+		System.out.println("I'm I'm Iron man"+a);
+		}
+	}
+
+}
