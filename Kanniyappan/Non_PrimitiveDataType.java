6b57b5b69bfe937376fe9ae77c7bbcc59a7f0865
selva10002 <119669635+selva10002@users.noreply.github.com>
2022-12-24 19:34:53 +0530

    Add files via upload

--- Kanniyappan/Non_PrimitiveDataType.java
+++ Kanniyappan/Non_PrimitiveDataType.java
@@ -1,16 +1,47 @@
 package datatype;
 
 
-/*Non-primitive data types are three types
+
+/*Non-primitive data types are three types (there )
+ * 
+ * the String And Array is classes
  * 
  * 1.String
+ *  
+ *  Syntax
+ *  
+ *  Data type Variable = value
+ *  
+ *  1 way
+ *  String a="Kanniyappan";
+ *  
+ *  2 way
+ *  
+ *  Data Type Variable;
+ *  
+ *  Variable=value;
+ *  
+ *  String c;
  * 
- * 				used to Store Multiple(Sequances) Characters(letters)  
+ *  
+ *  c="Kanniyappan";
+ * 				  
+ * 
+ * 
  * 
  * 2.Array[]
  * 
- *        array[] b={2,3,3};
+ * Data type Variable[]={value};
  * 
+ * example
+ * 
+ *    int number[]={1,2,3,4,5};
+ * 
+ * 
+ * 
+ *       
+ * 
+ * 3.UserDefine Data types
  *  
  *  * */
 public class Non_PrimitiveDataType 
@@ -18,11 +49,70 @@ public class Non_PrimitiveDataType
 
 	public static void main(String[] args)
 	{
-		//String
-			
-		String a="My name is Kanniyappan";
+		
+		//non primitive data type
+		
+		
+		//String data type Stores sequence of letter . within Double codes " "..
+		//1 st Way
+		String a="my ,name ,Kanniyappan";
+		
+		//2 nd way
+		
+		String b;
+		
+		b="Selva";
+		
 		
 		System.out.println(a);
-	}
+		
+		System.out.println(b);
+		
+		
+		//Array[] data type Store More Data within order..  
+		
+		//the important rule of Array[] is once the Array is declared the values of array is not be involved in increased and Decreased process 
+		
+		/*example
+		 * 
+		 * String name[]={"Kanniyappan","Selva","Seenivasan","dinagara"};
+		 * 
+		 * name[0]=Kanniyappan
+		 * name[1]=Selva
+		 * name[2]=Seenivasan
+		 * name[3]=dinagaran
+		 * 
+		 * 
+		 * 
+		 * */
+		
+		int Numbers[]= {1,2,4,5,6};
+		
+		//Numbers[0]=1
+		//Numbers[1]=2
+		//Numbers[2]=4
+		//Numbers[3]=5
+		//Numbers[4]=6
+		
+		float points[]= {1,2,3,4,5};
+		
+		
+		char letters[]= {'k','n','n','i','y','a','p','p','a','n'};
+		
+		//letters[0]=k
+		
+		String name[]= {"Kanniyappan","Selva","Seenivasan","dinagara"};	
+		
+		
+		System.out.println("String Values :"+a);
+		
+		System.out.println("Array int  numbers :"+Numbers[2]);
+		
+		System.out.println("Array float points :"+points[4]);
+		
+		System.out.println("Array char letters :"+letters[4]);
+		
+		System.out.println("Array String name :"+name[2]);
+		}
 
 }
